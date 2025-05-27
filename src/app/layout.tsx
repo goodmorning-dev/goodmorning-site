4b1e25d37c1ea099442b2eb1543495bcfbf289d7
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2025-05-27 15:07:56 +0300

    Fixed layout page render

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,15 +4,19 @@ import Footer from '@/components/Footer'
 
 export const metadata = {
   title: 'GoodMorning',
-  description: 'Web3 development studio'
+  description: 'Web3 development studio',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body className="bg-white text-gray-900">
+      <body>
         <Header />
-        <main className="min-h-screen px-4 md:px-8 py-6 pt-[110px]">{children}</main>
+        <main className="min-h-screen pt-[110px]">{children}</main>
         <Footer />
       </body>
     </html>
