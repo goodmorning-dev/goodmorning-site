f6ef707512a934de6f0f7f2bef2a6423c2a5883c
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2025-07-14 16:59:58 +0300

    Added service details section /TODO

---
+++ src/constants/web3Cto.ts
@@ -0,0 +1,40 @@
+import GearsIcon from '@/../public/icons/gears.png'
+import BriefcaseIcon from '@/../public/icons/briefcase.png'
+import EngineersIcon from '@/../public/icons/engineers.png'
+import TelegramIcon from '@/../public/icons/telegram.png'
+import JoystickIcon from '@/../public/icons/joystick.png'
+
+const web3Cto = [
+  {
+    icon: GearsIcon,
+    title: 'Non-technical founders & Entrepreneurs',
+    description:
+      'You focus on the vision - we handle the tech. From idea to execution, we translate concepts into working, scalable products, manage dev teams, and make sure nothing gets lost in translation.',
+  },
+  {
+    icon: BriefcaseIcon,
+    title: 'Investors & Venture capitalists',
+    description:
+      'We help de-risk your investments by ensuring projects are technically sound, scalable, and built for long-term growth.',
+  },
+  {
+    icon: EngineersIcon,
+    title: 'Early-stage startups',
+    description:
+      'Launching is tough. With the right partner, it becomes strategic. We help you choose the right stack, structure your architecture, and build the foundation your startup needs.',
+  },
+  {
+    icon: TelegramIcon,
+    title: 'Teams transitioning to Web3 or blockchain solutions',
+    description:
+      "Moving from Web2 to Web3? We'll guide your transition with expert insights, technical leadership, and real-world implementation.",
+  },
+  {
+    icon: JoystickIcon,
+    title: 'Startups seeking CTO-level guidance',
+    description:
+      "Need a tech lead without hiring a full-time CTO? We'll shape your roadmap, define your architecture, and ensure flawless execution.",
+  },
+]
+
+export default web3Cto
