65096ca40a124cb5392ec7f0ad1340e4e894594f
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2025-07-17 10:17:37 +0300

    Changed team card hover animation

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -19,14 +19,14 @@ export default function TeamSection({ short = false }: TeamSectionProps) {
             key={index}
             className="group relative h-[460px] cursor-pointer overflow-hidden rounded-[25px] transition-all duration-300"
           >
-            <div className="transition-inset absolute inset-[7px] z-0 rounded-[15px] bg-secondary duration-100 group-hover:inset-0" />
+            <div className="transition-inset absolute inset-[7px] z-0 rounded-[15px] bg-secondary duration-100" />
 
             <div className="absolute inset-0 z-0 before:absolute before:inset-0 before:z-10 before:rounded-[25px] before:bg-[linear-gradient(to_top,rgba(30,30,30,0.6),transparent)]">
               <Image
                 src={member.image}
                 alt={member.name}
                 fill
-                className="rounded-[25px] object-cover"
+                className="group-hover:clip-normal object-cover transition-[clip-path] duration-500 ease-in-out"
                 style={{
                   clipPath:
                     'polygon(0 0, calc(100% - 60px) 0, 100% 60px, 100% 100%, 0 100%)',
@@ -36,15 +36,14 @@ export default function TeamSection({ short = false }: TeamSectionProps) {
               />
             </div>
 
-            <div className="absolute bottom-4 z-10 w-[100%] py-4 text-center tracking-wider">
+            <div className="absolute bottom-4 z-10 w-[100%] py-4 text-center tracking-wider transition-opacity duration-300 group-hover:opacity-0">
               <h4 className="mb-3 text-center text-[30px] font-semibold">
                 {member.name}
               </h4>
               <p className="text-2xl text-primary">{member.position}</p>
             </div>
 
-            {/* TODO: bubbles? */}
-            <div className="absolute inset-0 z-40 flex items-center justify-center bg-secondary text-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+            <div className="absolute inset-0 z-40 flex items-center justify-center bg-secondary text-center opacity-0 transition-opacity duration-300 group-hover:opacity-80">
               <p className="px-6 text-[24px] leading-[34px] tracking-[0.7px]">
                 {member.hoverText}
               </p>
