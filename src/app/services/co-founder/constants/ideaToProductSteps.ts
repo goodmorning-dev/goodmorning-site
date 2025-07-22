3ed55806a8f0db9fc1b3c0727889b62d149487d1
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2025-07-22 16:17:01 +0300

    Fixed upper and smaller case letters

--- src/app/services/co-founder/constants/ideaToProductSteps.ts
+++ src/app/services/co-founder/constants/ideaToProductSteps.ts
@@ -1,37 +1,32 @@
 export const ideaToProductSteps = [
   {
     step: 'Step 1',
-    title: 'Vision & Goal Alignment',
-    description:
-      'Deep dive into your business and technical needs',
+    title: 'Vision & Goal alignment',
+    description: 'Deep dive into your business and technical needs',
     image: '/images/productSteps/step1.png',
   },
   {
     step: 'Step 2',
-    title: 'Tech Strategy & Roadmap',
-    description:
-      'Define stack, architecture & MVP milestones',
+    title: 'Tech strategy & Roadmap',
+    description: 'Define stack, architecture & MVP milestones',
     image: '/images/productSteps/step2.png',
   },
   {
     step: 'Step 3',
-    title: 'Assembling & Leading the Tech Team',
-    description:
-      'Build your squad and lead with agile workflows',
+    title: 'Assembling & Leading the tech team',
+    description: 'Build your squad and lead with agile workflows',
     image: '/images/productSteps/step3.png',
   },
   {
     step: 'Step 4',
-    title: 'Product Development',
-    description:
-      'Oversee dev process, iterate, and improve',
+    title: 'Product development',
+    description: 'Oversee dev process, iterate, and improve',
     image: '/images/productSteps/step4.png',
   },
   {
     step: 'Step 5',
-    title: 'Scaling & Fundraising Support',
-    description:
-      'Optimize for growth, prep for investor due diligence',
+    title: 'Scaling & Fundraising support',
+    description: 'Optimize for growth, prep for investor due diligence',
     image: '/images/productSteps/step5.png',
   },
 ]
