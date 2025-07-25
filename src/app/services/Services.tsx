2dc70d8b889b74428f4e455b81576c2b28ffd8a7
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2025-07-25 15:57:47 +0300

    Updated services page metadata

---
+++ src/app/services/Services.tsx
@@ -0,0 +1,319 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import Button from '@/components/Button'
+import ReadMoreLink from '@/components/ReadMoreLink'
+import Image from 'next/image'
+import steps from '@/app/services/constants/successAcordion'
+import { FaPlus, FaMinus } from 'react-icons/fa'
+
+export default function ServicesPage() {
+  const router = useRouter()
+
+  const [openIndex, setOpenIndex] = useState<number | null>(0)
+  const [closingIndex, setClosingIndex] = useState<number | null>(null)
+
+  const toggle = (index: number) => {
+    if (openIndex === index) {
+      setOpenIndex(null)
+      return
+    }
+
+    if (openIndex !== null) {
+      setClosingIndex(openIndex)
+      setOpenIndex(null)
+
+      setTimeout(() => {
+        setClosingIndex(null)
+        setOpenIndex(index)
+      }, 300)
+    } else {
+      setOpenIndex(index)
+    }
+  }
+
+  return (
+    <main>
+      {/* Hero Section */}
+      <section
+        className="relative mx-auto py-32 lg:py-52"
+        style={{
+          backgroundImage: "url('/images/services-bg.png')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <div className="mx-auto flex w-full max-w-8xl flex-col items-start justify-between gap-12 px-5 lg:flex-row lg:px-0">
+          <div>
+            <h1 className="header-1 max-w-3xl">Web3 development services</h1>
+          </div>
+
+          <div className="max-w-xl">
+            <p className="paragraph max-w-lg lg:text-[24px]">
+              Build smarter, scale faster with goodmorning - your Web3
+              development services company. Explore our blockchain solutions
+              designed to bring your ideas to life - from MVPs to full-scale
+              infrastructure.
+            </p>
+            <Button
+              className="mt-8"
+              variant="primary"
+              onClick={() => router.push('/contact')}
+            >
+              Get in Touch
+            </Button>
+          </div>
+        </div>
+      </section>
+
+      {/* Services Section */}
+      <section className="px-5 py-40 lg:px-0">
+        <h1 className="header-2 m-auto mb-8 max-w-5xl text-center lg:!text-[56px]">
+          <span className="gradient-underline">Unlock new levels</span> with our
+          blockchain development <span className="text-primary">services</span>
+        </h1>
+        <p className="paragraph m-auto mb-20 max-w-[870px] text-center !font-normal">
+          There's more than one way to get things done. So pick your fighter
+          from our menu and let's bring your ideas to life. Here's a quick look
+          at our service areas.
+        </p>
+        <div className="mx-auto flex max-w-8xl flex-col gap-12 lg:flex-row">
+          <div className="flex flex-col gap-12 lg:w-1/2">
+            <div
+              className="rounded-3xl bg-gray"
+              style={{
+                clipPath:
+                  'polygon(0 0, calc(100% - 50px) 0, 100% 50px, 100% 100%, 0 100%)',
+                WebkitClipPath:
+                  'polygon(0 0, calc(100% - 50px) 0, 100% 50px, 100% 100%, 0 100%)',
+                borderRadius: '20px',
+                overflow: 'hidden',
+              }}
+            >
+              <div className="p-8">
+                <h2 className="header-3 mb-5">
+                  Web3 technical co-founder as a service
+                </h2>
+                <p className="paragraph mb-6 !font-normal">
+                  Got the vision but need the technical brainpower? We step in
+                  as your technical co-founder helping you shape your product
+                  strategy, build scalable architecture, and lead your dev team
+                  from day one.
+                </p>
+                <ReadMoreLink className="mt-5" href="#" label="VIEW MORE" />
+              </div>
+              <Image
+                src="/images/service-cofounder.png"
+                width={700}
+                height={400}
+                alt="Web3 technical co-founder as a service"
+                className="w-full object-cover"
+              />
+            </div>
+            <div className="flex flex-col items-center gap-12 lg:flex-row">
+              <div
+                className="rounded-3xl bg-gray p-[30px] lg:w-1/2"
+                style={{
+                  clipPath:
+                    'polygon(0 0, calc(100% - 50px) 0, 100% 50px, 100% 100%, 0 100%)',
+                  WebkitClipPath:
+                    'polygon(0 0, calc(100% - 50px) 0, 100% 50px, 100% 100%, 0 100%)',
+                  borderRadius: '20px',
+                  overflow: 'hidden',
+                }}
+              >
+                <h2 className="header-3 mb-5 !text-[28px]">MVP development</h2>
+                <p className="paragraph mb-6 !text-[19px] !font-normal">
+                  Ideas are cool, but working products are cooler. We build
+                  rock-solid MVPs that get you to market quickly - without
+                  cutting corners.
+                </p>
+                <ReadMoreLink className="mt-5" href="#" label="VIEW MORE" />
+              </div>
+              <div
+                className="rounded-3xl bg-gray p-[30px] lg:w-1/2"
+                style={{
+                  clipPath:
+                    'polygon(0 0, calc(100% - 50px) 0, 100% 50px, 100% 100%, 0 100%)',
+                  WebkitClipPath:
+                    'polygon(0 0, calc(100% - 50px) 0, 100% 50px, 100% 100%, 0 100%)',
+                  borderRadius: '20px',
+                  overflow: 'hidden',
+                }}
+              >
+                <h2 className="header-3 mb-5 !text-[28px]">
+                  Web3 technical auditing
+                </h2>
+                <p className="paragraph mb-6 !text-[19px] !font-normal">
+                  Security is our forte, so you can count on us to deep dive
+                  into your infrastructure and tell you exactly what's solid and
+                  what needs fixing.
+                </p>
+                <ReadMoreLink className="mt-5" href="#" label="VIEW MORE" />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-12 lg:w-1/2">
+            <div
+              className="rounded-3xl bg-gray"
+              style={{
+                clipPath:
+                  'polygon(0 0, calc(100% - 50px) 0, 100% 50px, 100% 100%, 0 100%)',
+                WebkitClipPath:
+                  'polygon(0 0, calc(100% - 50px) 0, 100% 50px, 100% 100%, 0 100%)',
+                borderRadius: '20px',
+                overflow: 'hidden',
+              }}
+            >
+              <div className="flex flex-col lg:flex-row">
+                <Image
+                  src="/images/service-consultancy.png"
+                  width={340}
+                  height={340}
+                  alt="Web3 technical cof-ounder as a service"
+                  className="w-1/2 w-full object-cover"
+                />
+                <div className="p-8">
+                  <h2 className="header-3 mb-5 !text-[28px]">
+                    Web3 consultancy
+                  </h2>
+                  <p className="paragraph mb-6 !text-[19px] !font-normal">
+                    Finding the Web3 maze a bit overwhelming? Whatever your
+                    struggle, we break it down by offering expert guidance,
+                    adding confidence, and taking away the headaches.
+                  </p>
+                  <ReadMoreLink className="mt-5" href="#" label="VIEW MORE" />
+                </div>
+              </div>
+            </div>
+            <div
+              className="rounded-3xl bg-gray"
+              style={{
+                clipPath:
+                  'polygon(0 0, calc(100% - 50px) 0, 100% 50px, 100% 100%, 0 100%)',
+                WebkitClipPath:
+                  'polygon(0 0, calc(100% - 50px) 0, 100% 50px, 100% 100%, 0 100%)',
+                borderRadius: '20px',
+                overflow: 'hidden',
+              }}
+            >
+              <div className="p-8 pb-14">
+                <h2 className="header-3 mb-5">Staff augmentation</h2>
+                <p className="paragraph mb-6 !font-normal">
+                  If you're in need of an extra pair of hands (or maybe ten),
+                  let us plug in our Web3 devs, engineers and blockchain experts
+                  right into your team. Scaling up feels even better without the
+                  long hiring process.
+                </p>
+                <ReadMoreLink className="mt-5" href="#" label="VIEW MORE" />
+              </div>
+              <Image
+                src="/images/service-augmentation.png"
+                width={700}
+                height={400}
+                alt="Staff (team) augmentation"
+                className="w-full object-cover"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Success Section */}
+      <section className="bg-gray px-5 py-16 lg:px-0 lg:py-40">
+        <div className="mx-auto grid max-w-8xl grid-cols-1 items-center gap-12 md:grid-cols-2">
+          <div>
+            <img
+              src="/images/accordion-image.png"
+              alt="Our recipe for success"
+              className="w-full rounded-3xl object-cover"
+            />
+          </div>
+
+          <div>
+            <h2 className="header-2 mb-5 lg:!text-[56px]">
+              Our recipe for success
+            </h2>
+            <p className="paragraph mb-8 leading-relaxed">
+              There’s no room for guesswork in the Web3 world. Here’s our
+              battle-tested process that will take your idea from concept to
+              launch:
+            </p>
+
+            <div className="space-y-6">
+              {steps.map((step, index) => (
+                <div key={index}>
+                  <button
+                    onClick={() => toggle(index)}
+                    className="header-3 flex w-full items-center justify-between text-left tracking-[0.86px] focus:outline-none"
+                  >
+                    <span className="header-3 tracking-[0.86px]">
+                      <span className="text-primary">Step {index + 1}:</span>
+                      <span className="ml-2">{step.title}</span>
+                    </span>
+                    {openIndex === index ? (
+                      <FaMinus className="text-primary" size={20} />
+                    ) : (
+                      <FaPlus className="text-primary" size={20} />
+                    )}
+                  </button>
+
+                  <div
+                    className={`mt-2 overflow-hidden transition-all duration-300 ease-in-out ${
+                      openIndex === index
+                        ? 'max-h-96 opacity-100'
+                        : closingIndex === index
+                          ? 'max-h-0 opacity-0'
+                          : 'max-h-0 opacity-0'
+                    }`}
+                  >
+                    <p className="max-w-[590px] pt-4 text-[19px] italic leading-[26px] text-white/80">
+                      {step.content}
+                    </p>
+                  </div>
+                  <div
+                    className="my-8 w-full border-t"
+                    style={{ borderColor: '#d8d8d8', opacity: 0.3 }}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* CTA section */}
+      <section className="relative px-5 py-16 lg:px-0 lg:py-64">
+        <div
+          className="pointer-events-none absolute inset-0 bg-contain bg-center bg-no-repeat"
+          style={{ backgroundImage: "url('/images/cta-future-bg.png')" }}
+        />
+
+        <div className="relative z-10 mx-auto flex max-w-8xl flex-col items-center justify-between gap-10 lg:flex-row lg:gap-0">
+          <h2 className="header-2 max-w-xl">
+            {' '}
+            Let's elevate your
+            <br />
+            Web3 journey
+          </h2>
+
+          <div className="max-w-[500px] text-center lg:text-left">
+            <p className="paragraph leading-[1.33] tracking-[0.6px] lg:text-[24px]">
+              Ready to bring your ideas to life? Let's chat and explore how our
+              blockchain development services can help you level up in the Web3
+              space.
+            </p>
+            <Button
+              className="mt-9"
+              variant="primary"
+              onClick={() => router.push('/contact')}
+            >
+              Contact Us
+            </Button>
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
