de02598ebeae7ca5bf9cf576adf1b7bfd89c8635
Goran Stoyanov <stoyanovgoran@gmail.com>
2025-08-23 10:52:34 +0300

    Add: Job positions to sitemap.xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,3 +1,6 @@
+require('ts-node').register({ transpileOnly: true })
+const positions = require('./src/constants/positions').default
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://goodmorning.dev',
@@ -5,4 +8,12 @@ module.exports = {
   generateIndexSitemap: false,
   sitemapSize: 50000,
   exclude: ['/blog'],
+  additionalPaths: async (config) => {
+    const careerLinks = [
+      ...new Set(positions.flatMap(c => c.jobs.map(j => j.link))),
+    ]
+    return Promise.all(
+      careerLinks.map(p => config.transform(config, p))
+    )
+  },
 }
