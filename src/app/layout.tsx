fb5ff6041e98eb92ac6dac4c948924856f545f76
Goran Stoyanov <stoyanovgoran@gmail.com>
2025-08-23 11:37:02 +0300

    Add: meta images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,27 @@ import Script from 'next/script'
 export const metadata = {
   title: 'goodmorning | The Web3 Development Studio',
   description:
-    'Launch, scale, and secure your blockchain project with goodmorning - the Web3 development studio trusted by top teams and recognized by the Ethereum Foundation.'
+    'Launch, scale, and secure your blockchain project with goodmorning - the Web3 development studio trusted by top teams and recognized by the Ethereum Foundation.',
+  twitter: {
+    card: 'summary_large_image',
+    images: [`${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/images/meta/home-meta-image.png`,], // default image
+  },
+  openGraph: {
+    title: 'goodmorning | The Web3 Development Studio',
+    description:
+      'Launch, scale, and secure your blockchain project with goodmorning - the Web3 development studio trusted by top teams and recognized by the Ethereum Foundation.',
+    url: 'https://goodmorning.dev',
+    siteName: 'goodmorning.dev',
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/images/meta/home-meta-image.png`,
+        width: 1200,
+        height: 630,
+        alt: 'goodmorning preview',
+      },
+    ],
+    type: 'website',
+  },
 }
 
 export default function RootLayout({
