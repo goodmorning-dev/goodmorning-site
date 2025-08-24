a7777581de0b52b162c273b0ccae6b41fb5e26c0
Goran Stoyanov <stoyanovgoran@gmail.com>
2025-08-24 11:40:25 +0300

    Add: meta for legal pages

--- src/app/cookie-policy/page.tsx
+++ src/app/cookie-policy/page.tsx
@@ -2,6 +2,35 @@ import fs from 'fs'
 import path from 'path'
 import ReactMarkdown from 'react-markdown'
 
+export const metadata = {
+  title: 'Cookie Policy | goodmorning.dev',
+  description:
+    'Read goodmorning.dev’s Cookie Policy to learn how we use cookies and similar technologies to enhance your browsing experience.',
+  twitter: {
+    card: 'summary_large_image',
+    images: [
+      `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/images/meta/home-meta-image.png`,
+    ],
+  },
+  openGraph: {
+    title: 'Cookie Policy | goodmorning.dev',
+    description:
+      'Read goodmorning.dev’s Cookie Policy to learn how we use cookies and similar technologies to enhance your browsing experience.',
+    url: 'https://goodmorning.dev/cookie-policy',
+    siteName: 'goodmorning.dev',
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/images/meta/home-meta-image.png`,
+        width: 1200,
+        height: 630,
+        alt: 'goodmorning - cookie policy',
+      },
+    ],
+    type: 'website',
+  },
+}
+
+
 export default function CookiePolicyPage() {
   const filePath = path.join(
     process.cwd(),
--- src/app/sitemap/page.tsx
+++ src/app/sitemap/page.tsx
@@ -3,6 +3,34 @@ import path from 'path'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
+export const metadata = {
+  title: 'Sitemap | goodmorning.dev',
+  description:
+    'Explore the full sitemap of goodmorning.dev. Access all pages including services, careers, blog, and company info in one structured overview.',
+  twitter: {
+    card: 'summary_large_image',
+    images: [
+      `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/images/meta/home-meta-image.png`,
+    ],
+  },
+  openGraph: {
+    title: 'Sitemap | goodmorning.dev',
+    description:
+      'Explore the full sitemap of goodmorning.dev. Access all pages including services, careers, blog, and company info in one structured overview.',
+    url: 'https://goodmorning.dev/sitemap',
+    siteName: 'goodmorning.dev',
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/images/meta/home-meta-image.png`,
+        width: 1200,
+        height: 630,
+        alt: 'goodmorning - sitemap',
+      },
+    ],
+    type: 'website',
+  },
+}
+
 export default function SitemapPage() {
   const filePath = path.join(
     process.cwd(),
